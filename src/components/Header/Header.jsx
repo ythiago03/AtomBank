a9cc7b4fc817a5ea67dc33fb7773781eb28b0e90
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-06-05 18:58:47 -0300

    Feat: Creating menu hamburguer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Header.css';
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  const getMenuStyle = (menuOpened) => {
+    if(document.documentElement.clientWidth <= 1090){
+      return {top: menuOpened && '100px'};
+    }
+  };
+
+  const teste = (menuOpened, child) => { 
+    switch (child){
+    case 1:
+      return {
+        transform: menuOpened && 'rotate(45deg)',
+        top: menuOpened &&'8px'
+      };
+
+    case 2:
+      return {
+        opacity: menuOpened && '0',
+        transform: menuOpened && 'translateX(-100%)'
+      };
+
+    case 3:
+      return {
+        transform: menuOpened && 'rotate(-45deg)',
+        top: menuOpened &&'8px'
+      };
+
+    default:
+      break;  
+    }
+  };
+
   return (
     <header className="h-container">
 
@@ -15,28 +48,39 @@ const Header = () => {
         </span>
       </a>
 
-      <nav className="h-navbar">
-        <ul>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Features</a>
-          </li>
-          <li>
-            <a href="#">Pricing</a>
-          </li>
-          <li>
-            <a href="#">About Us</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="h-wrapper" style={getMenuStyle(menuOpened)}>
+        <nav className="h-navbar">
+          <ul>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+          </ul>
+        </nav>
 
-      <div className="h-buttons">
-        <button className="button buttonWhite">Log in</button>
-        <button className="button">Register</button>
+        <div className="h-buttons">
+          <button className="button buttonWhite">Log in</button>
+          <button className="button">Register</button>
+        </div>
       </div>
 
+      <div 
+        className="hamburguer"
+        onClick={() => setMenuOpened((previousValue) => !previousValue)}
+        
+      >
+        <span style={teste(menuOpened, 1)} ></span>
+        <span style={teste(menuOpened, 2)} ></span>
+        <span style={teste(menuOpened, 3)} ></span>
+      </div>
     </header>
   );
 };
